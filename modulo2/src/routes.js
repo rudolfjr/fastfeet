f84344cd51a6bad0b69d5eaa1926334417342dde
Rudolf Jr <rudinhok@gmail.com>
2020-03-21 20:59:30 -0300

    Aula 9

--- modulo2/src/routes.js
+++ modulo2/src/routes.js
@@ -1,9 +1,9 @@
-const { Router } = require('express');
+import { Router } from 'express';
 
 const routes = new Router();
 
 routes.get('/', (req, res) => {
-    return res.json({ message: 'Hellow World'});
+    return res.json({ message: 'Hellow  BBB'});
 });
 
-module.exports = routes;
+export default routes;
