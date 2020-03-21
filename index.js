2b020060d1d0fc3d8b632d2dc7bd8a5b70e6356e
Rudolf Jr <rudinhok@gmail.com>
2020-03-21 19:53:33 -0300

    Aula 2

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ const express = require('express');
 
 const server = express();
 
-server.get('/teste', (req, res) => {
-    return res.json({ message: 'Hello World' });
+server.get('/users/:id', (req, res) => {
+    //const nome = req.query.nome; // pegar uma tag da url, neste caso nome
+    const { id }  = req.params; // pegar parametros em rota, usando o params
+
+    return res.json({ message: `Hello ${id}` });
 });
 
 server.listen(3000);
